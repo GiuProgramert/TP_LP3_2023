1a26473577120c283d20678e26a6b075d06f6bcf
Marcelo Pauls <paulsmarcelo@live.com>
2023-09-17 17:26:48 -0400

    Se agrego el fork en el listing 3.7

--- src/Capitulo_3/listing_3.7.c
+++ src/Capitulo_3/listing_3.7.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include <signal.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 sig_atomic_t child_exit_status;
 
@@ -16,14 +19,25 @@ void clean_up_child_process (int signal_number)
 
 int main ()
 {
+    pid_t child_pid;
     /* Handle SIGCHLD by calling clean_up_child_process. */
     struct sigaction sigchld_action;
     memset (&sigchld_action, 0, sizeof (sigchld_action));
     sigchld_action.sa_handler = &clean_up_child_process;
     sigaction (SIGCHLD, &sigchld_action, NULL);
 
-    /* Now do things, including forking a child process. */
-    /* ... */
+    child_pid = fork ();
+    if (child_pid != 0) {
+      printf("Soy el proceso padre. A constitucion se espera hasta 7 segundos para que "
+           "el proceso hijo (probablemente) termina\n");
+    }
+    else {
+      printf("Soy el processo hijo. Probablemente voy a terminar ahora.\n");
+      exit(1);
+    }
+    
+    sleep(7);
 
+    printf("Mi proceso hijo termino con codigo exit %d\n", WEXITSTATUS(child_exit_status));
     return 0;
 }
